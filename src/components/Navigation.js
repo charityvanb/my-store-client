dd2f1bbb8880d186c4da7bff301450235c062083
charityvanb <charityvanb@gmail.com>
2019-07-02 15:43:01 -0600

    Added routing

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <nav className="App">
+        <Link to="/products">Products</Link>
+        <Link to="/">Home</Link>
+    </nav>
+  )
+}
+
+export default Navigation
