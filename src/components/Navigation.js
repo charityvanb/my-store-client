d69dbb8a784aed4cafece613f64ed8eb87654721
charityvanb <charityvanb@gmail.com>
2019-07-03 20:55:57 -0600

    Set up linting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Navigation = () => {
   return (
     <nav className="App">
-        <Link to="/products">Products</Link>
-        <Link to="/">Home</Link>
+      <Link to="/products">Products</Link>
+      <Link to="/">Home</Link>
     </nav>
   )
 }
