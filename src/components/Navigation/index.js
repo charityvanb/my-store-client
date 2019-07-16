01beb13c3bf75e8eebedf17ea1e801d0c35f0755
charityvanb <charityvanb@gmail.com>
2019-07-16 15:11:46 -0600

    added auth button fun times

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './navigation.module.scss'
+import { useAuth0 } from '../../react-auth0-wrapper'
 
 const Navigation = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
+
   return (
     <nav className={styles.navigation}>
       <Link to="/products">Products</Link>
       <Link to="/">Home</Link>
+      {!isAuthenticated && (
+        <button
+          onClick={() =>
+            loginWithRedirect({})
+          }
+        >
+          Log in
+        </button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
     </nav>
   )
 }
