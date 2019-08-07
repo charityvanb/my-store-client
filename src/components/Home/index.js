f8332c37be89d1450bdb8662d9aaa34b8c650645
charityvanb <charityvanb@gmail.com>
2019-08-06 23:32:53 -0600

    Linked home button and made it more responsive

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,13 +2,19 @@ import React from 'react'
 import styles from './home.module.scss'
 // import Loader from '../Loader'
 import ladiesHats from '../../images/ladiesHats.png'
+import { Link } from 'react-router-dom'
 
 
 const Home = () => {
   return (
-    <div className="styles.home">
-      <img src={ladiesHats} alt="Ladies' Hat Emporium"/>
-    </div>
+  // <div className="styles.home">
+      
+  //   <img src={ladiesHats} alt="Ladies' Hat Emporium" href="/products"/>
+  // </div>
+
+    <Link to="/products">
+      <img src={ladiesHats} alt="Ladies Hat Emporium" href="/products"/>
+    </Link>
   )
 }
 
