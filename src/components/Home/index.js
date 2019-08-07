8b2d3e9e62579b35d982b9ecda6774a2d95b926d
charityvanb <charityvanb@gmail.com>
2019-08-06 19:23:33 -0600

    Styled Home and made the illustration responsive

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import styles from './home.module.scss'
 // import Loader from '../Loader'
 import ladiesHats from '../../images/ladiesHats.png'
 
+
 const Home = () => {
   return (
-    <div className="App">
+    <div className="styles.home">
       <img src={ladiesHats} alt="Ladies' Hat Emporium"/>
     </div>
   )
