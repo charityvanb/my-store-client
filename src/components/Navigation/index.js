c9edfd6096e97edac5a91d84987fa87537440015
charityvanb <charityvanb@gmail.com>
2019-08-09 02:31:12 -0600

    removed commented out code

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -29,35 +29,3 @@ const Navigation = () => {
 }
 
 export default Navigation
-
-
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// import { useAuth0 } from '../../react-auth0-wrapper'
-
-// const Navigation = () => {
-//   const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
-
-//   return (
-//     <nav>
-//       <Link to='/'>Home</Link>
-//       <Link to='/products'>Products</Link>
-//       <Link to='/test'>Test API</Link>
-//       {isAuthenticated && <Link to='/profile'>Profile</Link>}
-//       {!isAuthenticated && (
-//         <button
-//           onClick={() =>
-//             loginWithRedirect({})
-//           }
-//         >
-//           Log in
-//         </button>
-//       )}
-
-//       {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-//     </nav>
-//   )
-// }
-
-// export default Navigation
